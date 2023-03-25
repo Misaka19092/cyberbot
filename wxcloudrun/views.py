ab509efa6d8cb3df6b803366182e376a711aa40e
Zoe Z <homura-2011@hotmail.com>
2023-03-25 20:06:06 +0800

    modified:   wxcloudrun/views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,22 +1,23 @@
 from flask import Flask, request
 import requests
-from run import app
+
+#app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 
 def wx():
-    # rec_data = request.get_json()
-    # openid = request.headers.get('x-wx-openid')
-    # message = rec_data['message']
-    # data = {
-    #     "touser": openid,
-    #     "msgtype": "text",
-    #     "text": {
-    #         "content": "Hello World,{}".format(message)
-    #     }
-    # }
-    # url = f"https://api.weixin.qq.com/cgi-bin/message/custom/send"
-    # r = requests.post(url, json=data)
+    rec_data = request.get_json()
+    openid = request.headers.get('x-wx-openid')
+    message = rec_data['message']
+    data = {
+        "touser": openid,
+        "msgtype": "text",
+        "text": {
+            "content": "Hello World,{}".format(message)
+        }
+    }
+    url = f"https://api.weixin.qq.com/cgi-bin/message/custom/send"
+    r = requests.post(url, json=data)
     return 'success'
 
 if __name__ == '__main__':
