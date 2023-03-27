a1ede70eace1182b43f404bcdb14eeb926e9fc1a
Zoe Z <homura-2011@hotmail.com>
2023-03-27 10:14:27 +0800

    modified:   wxcloudrun/views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import requests
+import requests,json
 from run import app
 
 #app = Flask(__name__)
@@ -7,26 +7,34 @@ from run import app
 @app.route('/', methods=['POST'])
 
 def wx():
+    # 接收微信发来的消息
     rec_data = request.get_json()
     openid = request.headers.get('x-wx-openid')
     message = rec_data['message']
-    data = {
+    # 转发给机器人并获得回复
+    wxurl = f"https://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=wx8bfa8275fb71767f"
+    chaturl='https://service-5eae8k92-1315204370.sg.apigw.tencentcs.com/v1/chat/completions'
+    chatheaders={
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer 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'
+    }
+    chatdata=json.dumps(
+    {
+    'model':'gpt-3.5-turbo',
+    'messages':[{'role':'user','content':message}]
+    }
+    )
+    chatresponse = requests.request("POST",chaturl,headers=chatheaders,data=chatdata)
+
+    # 解析回复内容，获得机器人答复
+    answer=chatresponse.json()['choices'][0]['message']['content']
+    response = {
         "touser": openid,
         "msgtype": "text",
         "text": {
             # "content": "Hello World,{}".format(message)
-        "content": "Hello World"
+        "content": answer
         }
     }
-    url = f"https://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=wx8bfa8275fb71767f"
-    r = requests.post(url, json=data)
-    return data
-
-@app.route('/test')
-def test():
-    """
-    :return: 返回index页面
-    """
-    return "data"
-# if __name__ == '__main__':
-#     app.run()
+    r = requests.post(wxurl, json=response)
+    return response
