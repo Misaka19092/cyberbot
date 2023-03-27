2823c5af26befe03584e335676511237aeb63f76
Zoe Z <homura-2011@hotmail.com>
2023-03-27 15:04:14 +0800

    modified:   wxcloudrun/views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -12,10 +12,11 @@ def wx():
     # print(rec_data)
     openid = request.headers.get('x-wx-openid')
     message = rec_data['message']
+    code=rec_dat['code']
     # 转发给机器人并获得回复
     wxurl = f"https://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=wx8bfa8275fb71767f"
     # os.environ["OPENAI_API_KEY"] = "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"
-    os.environ["OPENAI_API_KEY"] = message
+    os.environ["OPENAI_API_KEY"] = code
     chaturl='https://service-5eae8k92-1315204370.sg.apigw.tencentcs.com/v1/chat/completions'
     chatheaders={
         'Content-Type':'application/json',
@@ -25,7 +26,7 @@ def wx():
     {
     'model':'gpt-3.5-turbo',
     # 'messages':[{'role':'user','content':message}]
-    'messages':[{'role':'user','content':'Hello'}]
+    'messages':[{'role':'user','content':message}]
     }
     )
     chatresponse = requests.request("POST",chaturl,headers=chatheaders,data=chatdata)
