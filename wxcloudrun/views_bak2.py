867c7fc3afbb60155caa096fd4259caf92c77955
Zoe Z <homura-2011@hotmail.com>
2023-03-29 16:44:12 +0800

    modified:   wxcloudrun/__init__.py 	renamed:    wxcloudrun/views_test.py -> wxcloudrun/copy.py 	modified:   wxcloudrun/views.py 	renamed:    wxcloudrun/views_store.py -> wxcloudrun/views_bak2.py

--- wxcloudrun/views_bak2.py
+++ wxcloudrun/views_bak2.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,g
+from flask import Flask, request
 import requests,json,os
 from run import app
 
@@ -9,14 +9,16 @@ from run import app
 def wx():
     # 接收微信发来的消息
     rec_data = request.get_json()
-    g.openid = request.headers.get('x-wx-openid')
-    g.sessionid=rec_data['sessionid']
-    message0=rec_data['message']
-    message1='['+message0+']'
-    message = json.loads(message1)
+    # print(rec_data)
+    openid = request.headers.get('x-wx-openid')
+    msg='['+rec_data['message']+']'
+    message = json.loads(msg)
     code=rec_data['code']
+    # wxurl = f"https://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=wx8bfa8275fb71767f"
+
+    # 转发给机器人并获得回复
+    # os.environ["OPENAI_API_KEY"] = "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"
     os.environ["OPENAI_API_KEY"] = code
-    #只有通过前段回传的方式key才不会失效
     chaturl='https://service-5eae8k92-1315204370.sg.apigw.tencentcs.com/v1/chat/completions'
     chatheaders={
         'Content-Type':'application/json',
@@ -25,28 +27,18 @@ def wx():
     chatdata=json.dumps(
     {
     'model':'gpt-3.5-turbo',
+    # 'messages':[{'role':'user','content':message}]
     'messages':message
     }
     )
     chatresponse = requests.request("POST",chaturl,headers=chatheaders,data=chatdata)
+    # print(chatresponse)
     # 解析回复内容，获得机器人答复
     if 'choices' in chatresponse.json():
         answer=chatresponse.json()['choices'][0]['message']['content']
-        g.message2='['+message0+',{"role":"assistant","content":"'+answer+'"}'+']'
     else:
         answer=chatresponse.json()['error']['code']
     response = {
         "content": answer
         }
-    return response
-
-@app.after_request
-def after(response):
-    app_root = os.path.dirname(os.path.abspath(__file__))
-    file_dir=app_root+'\\'+g.openid
-    file_path=file_dir+'\\'+g.sessionid+'.txt'
-    if not os.path.exists(file_dir):
-        os.mkdir(file_dir)
-    with open(file_path, "w",encoding='utf-8') as file:
-        file.write(g.message2)
     return response
