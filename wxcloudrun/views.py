ed9d623ed2ebfe22c6224eb5bf6b3c6ae675acd7
Zoe Z <homura-2011@hotmail.com>
2023-03-29 15:29:20 +0800

    modified:   wxcloudrun/views.py 	modified:   wxcloudrun/views_bak.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request,g
 import requests,json,os
 from run import app
 
@@ -9,16 +9,14 @@ from run import app
 def wx():
     # 接收微信发来的消息
     rec_data = request.get_json()
-    # print(rec_data)
-    openid = request.headers.get('x-wx-openid')
-    msg='['+rec_data['message']+']'
-    message = json.loads(msg)
+    g.openid = request.headers.get('x-wx-openid')
+    g.sessionid=rec_data['sessionid']
+    message0=rec_data['message']
+    message1='['+g.message+']'
+    message = json.loads(message1)
     code=rec_data['code']
-    # wxurl = f"https://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=wx8bfa8275fb71767f"
-
-    # 转发给机器人并获得回复
-    # os.environ["OPENAI_API_KEY"] = "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"
     os.environ["OPENAI_API_KEY"] = code
+    #只有通过前段回传的方式key才不会失效
     chaturl='https://service-5eae8k92-1315204370.sg.apigw.tencentcs.com/v1/chat/completions'
     chatheaders={
         'Content-Type':'application/json',
@@ -27,18 +25,26 @@ def wx():
     chatdata=json.dumps(
     {
     'model':'gpt-3.5-turbo',
-    # 'messages':[{'role':'user','content':message}]
     'messages':message
     }
     )
     chatresponse = requests.request("POST",chaturl,headers=chatheaders,data=chatdata)
-    # print(chatresponse)
     # 解析回复内容，获得机器人答复
     if 'choices' in chatresponse.json():
         answer=chatresponse.json()['choices'][0]['message']['content']
+        g.message2='['+message0+',{"role":"assistant","content":"'+answer+'"}'+']'
     else:
         answer=chatresponse.json()['error']['code']
     response = {
         "content": answer
         }
     return response
+
+@app.after_request
+def store():
+    file_dir='./'+g.openid
+    file_path=file_dir+'/'+g.sessionid+'.log'
+    if not os.path.exists(file_dir):
+        os.mkdir(file_dir)
+    with open(file_path, "w",encoding='utf-8') as file:
+        file.write(g.message2)
