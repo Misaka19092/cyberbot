d3175c3ef1b28be603a5394ff6d833156aef9a34
Zoe Z <homura-2011@hotmail.com>
2023-03-29 16:12:17 +0800

    modified:   wxcloudrun/views.py 	modified:   wxcloudrun/views_bak.py 	new file:   wxcloudrun/views_copy.py

--- wxcloudrun/views_bak.py
+++ wxcloudrun/views_bak.py
@@ -1,44 +1,66 @@
-from flask import Flask, request
-import requests,json,os
+from datetime import datetime
+from flask import render_template, request
 from run import app
+from wxcloudrun.dao_bak import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
+from wxcloudrun.model_bak import Counters
+from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
-#app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
+def index():
+    """
+    :return: 返回index页面
+    """
+    return render_template('index.html')
 
-def wx():
-    # 接收微信发来的消息
-    rec_data = request.get_json()
-    # print(rec_data)
-    openid = request.headers.get('x-wx-openid')
-    msg='['+rec_data['message']+']'
-    message = json.loads(msg)
-    code=rec_data['code']
-    # wxurl = f"https://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=wx8bfa8275fb71767f"
 
-    # 转发给机器人并获得回复
-    # os.environ["OPENAI_API_KEY"] = "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"
-    os.environ["OPENAI_API_KEY"] = code
-    chaturl='https://service-5eae8k92-1315204370.sg.apigw.tencentcs.com/v1/chat/completions'
-    chatheaders={
-        'Content-Type':'application/json',
-        'Authorization': 'Bearer {}'.format(os.environ["OPENAI_API_KEY"])
-    }
-    chatdata=json.dumps(
-    {
-    'model':'gpt-3.5-turbo',
-    # 'messages':[{'role':'user','content':message}]
-    'messages':message
-    }
-    )
-    chatresponse = requests.request("POST",chaturl,headers=chatheaders,data=chatdata)
-    # print(chatresponse)
-    # 解析回复内容，获得机器人答复
-    if 'choices' in chatresponse.json():
-        answer=chatresponse.json()['choices'][0]['message']['content']
+@app.route('/api/count', methods=['POST'])
+def count():
+    """
+    :return:计数结果/清除结果
+    """
+
+    # 获取请求体参数
+    params = request.get_json()
+
+    # 检查action参数
+    if 'action' not in params:
+        return make_err_response('缺少action参数')
+
+    # 按照不同的action的值，进行不同的操作
+    action = params['action']
+
+    # 执行自增操作
+    if action == 'inc':
+        counter = query_counterbyid(1)
+        if counter is None:
+            counter = Counters()
+            counter.id = 1
+            counter.count = 1
+            counter.created_at = datetime.now()
+            counter.updated_at = datetime.now()
+            insert_counter(counter)
+        else:
+            counter.id = 1
+            counter.count += 1
+            counter.updated_at = datetime.now()
+            update_counterbyid(counter)
+        return make_succ_response(counter.count)
+
+    # 执行清0操作
+    elif action == 'clear':
+        delete_counterbyid(1)
+        return make_succ_empty_response()
+
+    # action参数错误
     else:
-        answer=chatresponse.json()['error']['code']
-    response = {
-        "content": answer
-        }
-    return response
+        return make_err_response('action参数错误')
+
+
+@app.route('/api/count', methods=['GET'])
+def get_count():
+    """
+    :return: 计数的值
+    """
+    counter = Counters.query.filter(Counters.id == 1).first()
+    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
